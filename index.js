d9e4f9dc22ce6d5150c8f3cb259dc9ab7fa81b0b
unknown <ahassan2001@msn.com>
2022-08-30 19:56:50 -0400

    adding html and js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//Aligned the header at the top of the page to the left
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+//Rounding off the images on the page
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = "50px"
+}
+
+//Aligned all dog names to the left
+let dogNames = document.querySelectorAll('.dog-name')
+
+//Changed the color of the footer text
+let footer = document.querySelector('.footer')
+footer.style.color = "blue"
+footer.style.borderStyle = "solid"
 
 // Select some elements...
